b8047054114ceed982334a7aca64d53816f20e7b
Geoffrey Gane <geoffreygane@gmail.com>
2020-12-30 01:29:51 +0100

    feat(fizzbuzz): handle edge cases

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,21 +1,33 @@
 package fizzbuzz
 
-import "strconv"
+import (
+	"errors"
+	"strconv"
+)
 
-func doFizzBuzz(int1, int2, limit int, str1, str2 string) string {
-	result := "1"
+const start = 1
+const max = 100
+
+func doFizzBuzz(int1, int2, limit int, str1, str2 string) (string, error) {
+	result := strconv.Itoa(start)
+	separator := ","
+
+	if limit < start || limit > max {
+		return "", errors.New(
+			"limit must be between" + result + " and " + strconv.Itoa(max))
+	}
 
 	for i := 2; i <= limit; i++ {
 		if i%int1 == 0 && i%int2 == 0 {
-			result += "," + str1 + str2
+			result += separator + str1 + str2
 		} else if i%int1 == 0 {
-			result += "," + str1
+			result += separator + str1
 		} else if i%int2 == 0 {
-			result += "," + str2
+			result += separator + str2
 		} else {
-			result += "," + strconv.Itoa(i)
+			result += separator + strconv.Itoa(i)
 		}
 	}
 
-	return result
+	return result, nil
 }
