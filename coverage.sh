8981ee0b25eba6b1b95cb129b20d02be7e0f473a
Geoffrey Gane <geoffreygane@gmail.com>
2021-01-03 01:06:11 +0100

    ci(gen): check codecov over hardcoded packages

--- coverage.sh
+++ coverage.sh
@@ -3,10 +3,14 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d
+declare -a packages=("fb" "utils")
+
+for d in "${packages[@]}"; do
+    cd $d
+    go test -race -coverprofile=profile.out -covermode=atomic
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
     fi
+    cd -
 done
