9ea845fcdcedb6da01474b4a22809e338bf46805
Geoffrey Gane <geoffreygane@gmail.com>
2021-01-03 23:13:57 +0100

    test(utils): test GetTopRequestFromList()

--- utils/helper_test.go
+++ utils/helper_test.go
@@ -1,7 +1,11 @@
 package utils
 
 import (
+	"reflect"
 	"testing"
+
+	"github.com/Geoffrey42/fizzbuzz/models"
+	"github.com/go-redis/redis"
 )
 
 func TestBuildMemberFromParams(t *testing.T) {
@@ -19,4 +23,52 @@ func TestBuildMemberFromParams(t *testing.T) {
 	if got != want {
 		t.Errorf("got %s but want %s", got, want)
 	}
+}
+
+func TestGetTopRequestFromList(t *testing.T) {
+	t.Run("The is one top request", func(t *testing.T) {
+		var topRequests = []redis.Z{
+			{
+				Score:  42,
+				Member: "3-5-16-fizz-buzz",
+			},
+			{
+				Score:  21,
+				Member: "2-8-75-Perceval-Karadoc",
+			},
+		}
+
+		got, _ := GetTopRequestFromList(topRequests)
+		want := models.Stat{
+			Hit:   42,
+			Int1:  3,
+			Int2:  5,
+			Limit: 16,
+			Str1:  "fizz",
+			Str2:  "buzz",
+		}
+
+		if !reflect.DeepEqual(*got, want) {
+			t.Errorf("got %v but want %v", got, want)
+		}
+	})
+
+	t.Run("There are several top requests", func(t *testing.T) {
+		var topRequests = []redis.Z{
+			{
+				Score:  42,
+				Member: "3-5-16-fizz-buzz",
+			},
+			{
+				Score:  42,
+				Member: "2-8-75-Perceval-Karadoc",
+			},
+		}
+
+		_, err := GetTopRequestFromList(topRequests)
+
+		if err == nil {
+			t.Errorf("got no error but expect one")
+		}
+	})
 }
